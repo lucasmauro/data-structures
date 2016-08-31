ec6c80c42983b01912eaf866318bb512691373ac
Lucas Mauro <lucasmaurodesouza@gmail.com>
2016-08-31 20:14:01 -0300

    Array List added

---
+++ array-structures/array_list.h
@@ -0,0 +1,186 @@
+/* Copyright [2016] <Lucas Mauro de Souza>
+ * array_list.h
+ */
+
+/*!
+ * Array List
+ *
+ * Represents a list, a structure in which elements may be added at the beginning, top or middle
+ * of the structure. The number of elements stored can always be verified.
+ *
+ * @see ArrayList::push()
+ * @see ArrayList::push_back()
+ * @see ArrayList::push_front()
+ * @see ArrayList::pop()
+ * @see ArrayList::pop_back()
+ * @see ArrayList::pop_front()
+ * @see ArrayList::size()
+ */
+#ifndef STRUCTURES_ARRAY_LIST_H
+#define STRUCTURES_ARRAY_LIST_H
+
+#include <cstdint> //!< std::size_t included
+#include <stdexcept> //!< C++ exceptions included
+
+
+namespace structures {
+
+    template<typename T>
+        class ArrayList {
+        public:
+            /*!
+            * Builds an empty list with its default maximum size.
+            *
+            * @see ArrayList::DEFAULT_MAX
+            */
+            ArrayList();
+
+            /*!
+            * Builds an empty list with given size.
+            */
+            ArrayList(std::size_t max_size);
+
+            /*!
+            * Destructs the list andi ts elements.
+            */
+            ~ArrayList();
+
+            /*!
+            * Clears the list.
+            */
+            void clear();
+
+            /*!
+            * Inserts an element at the back of the list.
+            */
+            void push_back(T data);
+
+            /*!
+            * Inserts an element at the front of the list.
+            */
+            void push_front(T data);
+
+            /*!
+            * Inserts an element at the given index.
+            */
+            void insert(T data, std::size_t index);
+
+            /*!
+            * Inserts an element according to its sorting settings.
+            */
+            void insert_sorted(T data);
+
+            /*!
+            * Removes an element at the given index.
+            */
+            T pop(std::size_t index);
+
+            /*!
+            * Removes the element at the top of the list.
+            */
+            T pop_back();
+
+            /*!
+            * Removes the element at the front of the list.
+            */
+            T pop_front();
+
+            /*!
+            * Removes a given element.
+            */
+            void remove(T data);
+
+            /*!
+            * Returns whether the list is full or not.
+            */
+            bool full() const;
+
+            /*!
+            * Returns wether the list is empty or not.
+            */
+            bool empty() const;
+
+            /*!
+            * Returns whether the list contains an element or not.
+            */
+            bool contains(const T& data) const;
+
+            /*!
+            * Returns the position of a given element.
+            */
+            std::size_t find(const T& data) const;
+
+            /*!
+            * Returns the size of the list.
+            */
+            std::size_t size() const;
+
+            /*!
+            * Returns the maximum size of the list.
+            */
+            std::size_t max_size() const;
+
+            /*!
+            * Returns the element stored at the given index, verifying the index boundaries.
+            */
+            T& at(std::size_t index);
+
+            /*!
+            * Returns the element stored at the given index, without verifying the index boundaries.
+            */
+            T& operator[](std::size_t index);
+
+            /*!
+            * Returns the constant element stored at the given index, verifying the index boundaries.
+            */
+            const T& at(std::size_t index) const;
+
+            /*!
+            * Returns the constant element stored at the given index, without verifying the index boundaries.
+            */
+            const T& operator[](std::size_t index) const;
+
+        private:
+            T* contents; //!< The array containing the elements.
+            std::size_t size_; //!< The current size of the list.
+            std::size_t max_size_; //!< The maximum possible size of the list.
+
+            const static auto DEFAULT_MAX = 10u; //!< The default maximum size o the list.
+            
+            /*!
+            * Verifies whether the list is not full. Throws an exception otherwise.
+            */
+            void assureNotFull() {
+                if (this->full())
+                    throw std::out_of_range("The list is full.");
+            }
+            
+            /*!
+            * Verifies whether the list is not empty. Throws an exception otherwise.
+            */
+            void assureNotEmpty() {
+                if (this->empty())
+                    throw std::out_of_range("The list is empty.");
+            }
+            
+            /*!
+            * Verifies whether an index is fit to be used for exclusion. Throws an exception if negative.
+            */
+            void assureValidPopPosition(std::size_t index) {
+                if (index >= this->max_size_ || index < 0 || index >= this->size_)
+                    throw std::out_of_range("Invalid position.");
+            }
+            
+            /*!
+            * Verifies whether an index is fit to be used for insertion. Throws an exception if negative.
+            */
+            void assureValidInsertPosition(std::size_t index) {
+                if (index >= this->max_size_ || index < 0 || index > this->size_)
+                    throw std::out_of_range("Invalid position.");
+            }
+    };
+
+}
+
+#include "array_list.ipp"
+#endif
---
+++ array-structures/array_list.ipp
@@ -0,0 +1,154 @@
+#ifndef STRUCTURES_ARRAY_LIST_IPP
+#define STRUCTURES_ARRAY_LIST_IPP
+
+    using namespace structures;
+
+    template<typename T>
+    ArrayList<T>::ArrayList() {
+        this->ArrayList(this->DEFAULT_MAX);
+
+    }
+
+    template<typename T>
+    ArrayList<T>::ArrayList(std::size_t max_size) {
+        this->contents = new T[max_size];
+        this->size_ = 0;
+        this->max_size_ = max_size;
+    }
+
+    template<typename T>
+    ArrayList<T>::~ArrayList() {
+        delete this->contents;
+    }
+
+    template<typename T>
+    void ArrayList<T>::clear() {
+        this->size_ = 0;
+    }
+
+    template<typename T>
+    void ArrayList<T>::push_back(T data) {
+        this->insert(data, this->size_);
+    }
+
+    template<typename T>
+    void ArrayList<T>::push_front(T data) {
+        this->insert(data, 0);
+    }
+
+
+    template<typename T>
+    void ArrayList<T>::insert(T data, std::size_t index) {
+        this->assureNotFull();
+        this->assureValidInsertPosition(index);
+
+        for (auto i = this->size_; i > index; i--) {
+            this->contents[i] = this->contents[i-1];
+        }
+
+        this->size_++;
+        this->contents[index] = data;
+    }
+
+    template<typename T>
+    void ArrayList<T>::insert_sorted(T data) {
+        this->assureNotFull();
+
+        auto i = 0;
+        while (i < this->size_ && data > this->contents[i]) {
+            i++;
+        }
+
+        this->insert(data, i);
+    }
+
+    template<typename T>
+    T ArrayList<T>::pop(std::size_t index) {
+        this->assureNotEmpty();
+        this->assureValidPopPosition(index);
+        
+        auto i = index;
+        T data = this->contents[index];
+
+        while (i < this->size_) {
+            this->contents[i] = this->contents[i+1];
+            i++;
+        }
+        
+        this->size_--;
+        return data;
+    }
+
+    template<typename T>
+    T ArrayList<T>::pop_back() {
+        return this->pop(this->size_-1);
+    }
+
+    template<typename T>
+    T ArrayList<T>::pop_front() {
+        return this->pop(0);
+    }
+
+    template<typename T>
+    void ArrayList<T>::remove(T data) {
+        auto position = this->find(data);
+        this->pop(position);
+    }
+
+    template<typename T>
+    bool ArrayList<T>::full() const {
+        return this->size_ == this->max_size_;
+    }
+
+    template<typename T>
+    bool ArrayList<T>::empty() const {
+        return this->size_ == 0;
+    }
+
+    template<typename T>
+    bool ArrayList<T>::contains(const T& data) const {
+        return this->find(data) != this->size_;
+    }
+
+    template<typename T>
+    std::size_t ArrayList<T>::find(const T& data) const {
+        for (auto i = 0; i <= this->size_; i++) {
+            if (this->contents[i] == data)
+                return i;
+        }
+        return this->size_;
+    }
+
+    template<typename T>
+    std::size_t ArrayList<T>::size() const {
+        return this->size_;
+    }
+
+    template<typename T>
+    std::size_t ArrayList<T>::max_size() const {
+        return this->max_size_;
+    }
+
+    template<typename T>
+    T& ArrayList<T>::at(std::size_t index) {
+        this->assureValidPopPosition(index);
+        return this->contents[index];
+    }
+
+    template<typename T>
+    T& ArrayList<T>::operator[](std::size_t index) {
+        return this->contents[index];
+    }
+
+    template<typename T>
+    const T& ArrayList<T>::at(std::size_t index) const {
+        this->assureValidPopPosition(index);
+        return this->content[index];
+    }
+
+    template<typename T>
+    const T& ArrayList<T>::operator[](std::size_t index) const {
+        return this->content[index];
+    }
+
+#endif
